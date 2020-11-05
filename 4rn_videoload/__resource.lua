42af47d77bdd3bbe5293b09218280c4d58a09f00
halicsarnold <halicsarnold@gmail.com>
2020-11-05 23:02:35 +0100

    Add files via upload

---
+++ 4rn_videoload/__resource.lua
@@ -0,0 +1,27 @@
+----------------------------------------------------
+-- 4rn_videoloadscreen | MANIFEST (c)4rn_OLD 2020 --
+----------------------------------------------------
+--
+--	Created by Arnold Halics
+--
+--	This work is licensed under MIT.
+--
+--	If problem this script, please write e-mail: nemerdekel@sohivatal.hu
+--
+
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+version '1.0'
+
+description '4rn_videoloadscreen'
+
+files {
+    'index.html',
+    'config.js',
+    'css/min.css',
+    'images/logo.png',
+    'images/pattern.png',
+    'js/plugins.js',	
+}
+
+loadscreen 'index.html'
---
+++ 4rn_videoload/config.js
@@ -0,0 +1,25 @@
+/**
+|
+|	Fivem [4rn_videoloadscreen] 1.0
+|
+**/
+
+
+/** Background setting **/
+
+var youtubeurl = "https://www.youtube.com/watch?v=pHr8gRWz9uo" 			/** Add YouTube url **/
+var youtubemute = "off"													/** on: mute youtube video audio **/
+var youtubeloop = "on"													/** on: loop youtube video **/
+
+
+/**
+TEXT CONFIG
+secoundary color: 	add <span> </span> tag
+line feed: 			add <br> tag
+example:			4rn_<span>ROLEPLAY</span><br>rp server<br>INTRO
+**/
+
+var slide1 = "4rn_<span>ROLEPLAY</span><br>rp server<br>INTRO"			/** Slide 1 text **/
+var slide2 = "simple<br><span>lightweight </span><br>cool"				/** Slide 2 text **/
+var slide3 = "<span>GOOD</span><br>CHOISE"								/** Slide 3 text **/
+var comments = "Server is loading! Wait a minute / or two :D /"			/** Bottom comment text **/
